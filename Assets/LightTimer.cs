886c8b436760c1a12b172f922a9a11d0b3c7db72
Mārtiņš Ciekurs <mciekurs@outlook.com>
2018-04-21 01:07:07 +0300

    StartCoroutine added

---
+++ Assets/LightTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTimer : MonoBehaviour
+{
+
+    public Light light;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    StartCoroutine(Example());
+	    light = GetComponent<Light>();
+	}
+
+    IEnumerator Example()
+    {
+        while (light.intensity != 0f) 
+        {
+            light.intensity -= .005f;
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+        
+
+    }
+
+}
